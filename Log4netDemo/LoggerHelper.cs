abf7d6fb7176686c307995c39c28b8d4269607ea
Max Geng <leegxf@gmail.com>
2021-06-19 22:32:42 +0800

    添加项目文件。

---
+++ Log4netDemo/LoggerHelper.cs
@@ -0,0 +1,47 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4netDemo
+{
+    public class LoggerHelper
+    {
+        public static readonly log4net.ILog loginfo = log4net.LogManager.GetLogger("loginfo");  // 这里的 loginfo 和 log4net.config 里的名字要一样
+        public static readonly log4net.ILog logerror = log4net.LogManager.GetLogger("logerror");// 这里的 logerror 和 log4net.config 里的名字要一样
+
+        LoggerHelper()
+        {
+            
+        }
+        public static void SetConfig()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public static void SetConfig(FileInfo configFile)
+        {
+            log4net.Config.XmlConfigurator.Configure(configFile);
+        }
+
+        public static void WriteLog(string info)
+        {
+            if (loginfo.IsInfoEnabled)
+            {
+                loginfo.Info(info);
+            }
+        }
+
+        public static void WriteError(string info, Exception ex)
+        {
+            if (logerror.IsErrorEnabled)
+            {
+                logerror.Error(info, ex);
+            }
+        }
+
+    }
+}
---
+++ Log4netDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4netDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LoggerHelper.WriteLog("Main1");
+                throw new Exception(message: "this is test message");
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.WriteError("",ex);
+            }
+
+            try
+            {
+                LoggerHelper.WriteLog("Main2");
+                throw new Exception(message: "this is test message1");
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.WriteError("", ex);
+            }
+            Console.Read();
+        }
+    }
+}
